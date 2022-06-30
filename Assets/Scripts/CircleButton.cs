18d77bec18cb5be626a2618f5d12d1dfe8702d30
fuji250 <fuji8.goo@gmail.com>
2022-07-01 03:09:23 +0900

    First

---
+++ Assets/Scripts/CircleButton.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CircleButton :  Button, ICanvasRaycastFilter{
+
+	[SerializeField]
+	float radius = 64f;
+
+	public bool IsRaycastLocationValid (Vector2 sp, Camera eventCamera)
+	{
+		return Vector2.Distance(sp, transform.position) < radius;
+	}
+}
---
+++ Assets/Scripts/NextPlayerText.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextPlayerText : MonoBehaviour
+{
+    GameController gameController;
+    Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.Find("GameManager").GetComponent<GameController>();
+        text = this.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string colorText = "";
+        switch (gameController.player)
+        {
+            case GameController.COLOR.BLACK:
+                colorText = "çï";
+                break;
+            case GameController.COLOR.WHITE:
+                colorText = "îí";
+                break;
+            default:
+                break;
+        }
+        text.text = "éüÇÃî‘ÅF" + colorText;
+    }
+}
